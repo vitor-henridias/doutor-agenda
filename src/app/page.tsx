230a1b2fead07681d69e42efb1a2dccde788f583
vitor-henridias <vhdias.dev@gmail.com>
2025-05-26 20:04:36 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="flex h-screen items-center justify-center text-red-500">
-        Hello, World!
-      </h1>
+      <Button>Bootcamp</Button>
     </div>
   );
 }
