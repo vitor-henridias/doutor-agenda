be68f8fc737f020bf6ca69e219bccc230ffbb2e8
vitor-henridias <vhdias.dev@gmail.com>
2025-05-26 18:39:02 -0300

    chore: add prettier and eslint setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex h-screen items-center justify-center text-red-500">
+        Hello, World!
+      </h1>
+    </div>
+  );
+}
